4bca8b4e505752c37de6f1e0446d48d3a4aa0196
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2023-10-22 16:58:17 +0200

    Privacy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,41 @@
+export default async function Page() {
+    return (
+        <>
+            <div className="w-full h-full flex flex-col justify-center">
+                <h1 className="text-2xl font-bold text-center">Privacy</h1>
+                <p className="text-xs font-light leading-snug text-center">
+                    At GenieusAI, your privacy is of utmost importance to us. This
+                    Privacy Policy outlines how we collect and handle your data when
+                    you use our chatbot powered by the GPT-4 Large Language Model
+                    (LLM). Data Collection and Use Prompt Messages: GenieusAI
+                    collects and stores prompt messages provided by users solely for
+                    the purpose of delivering a seamless chat experience. These
+                    messages are used to generate responses and to provide users
+                    with access to their previous chat sessions. Data Security
+                    Secure Storage: GenieusAI ensures the secure storage of all
+                    data, particularly sensitive information. We employ
+                    state-of-the-art security measures to safeguard your data
+                    against unauthorized access or disclosure. Data Sharing
+                    Non-Personally Identifiable Information: GenieusAI does not
+                    sell, share, or trade any information that personally identifies
+                    users. We are committed to keeping your data confidential and
+                    protected. Third Parties GenieusAI does not engage with
+                    third-party entities for the purpose of sharing or selling user
+                    data. Your information is treated with the utmost respect for
+                    your privacy. Consent By using GenieusAI, you consent to the
+                    practices described in this Privacy Policy. If you do not agree
+                    with these practices, please refrain from using our chatbot.
+                    Changes to Privacy Policy GenieusAI may update this Privacy
+                    Policy from time to time to reflect changes in technology or
+                    legal requirements. Any significant changes will be communicated
+                    to users through appropriate channels. Contact Information If
+                    you have any questions or concerns about this Privacy Policy,
+                    please contact us at tinochaminuka@gmail.com. Last updated:
+                    22/10/23. Your trust is vital to us. Rest assured that we are
+                    committed to protecting your data and ensuring your privacy.
+                    Thank you for choosing GenieusAI.
+                </p>
+            </div>
+        </>
+    );
+}
